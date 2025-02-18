47e710e8a36140c54194c2e6d1686509ee344f58
TahmidShawn7 <md.tahmid022@gmail.com>
2025-02-18 12:10:41 +0600

    completed project

--- project_2/src/routes/product.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { insertSampleProducts } from "../controllers/product.controller.js";
-
-const router = express.Router();
-
-router.post("/add", insertSampleProducts);
-
-export default router;
--- project_2_aggregation_pipeline/src/models/product.model.js
+++ project_2_aggregation_pipeline/src/models/product.model.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     name: String,
     category: String,
     price: Number,
-    isStock: Boolean,
+    inStock: Boolean,
     tags: [String],
 });
 
